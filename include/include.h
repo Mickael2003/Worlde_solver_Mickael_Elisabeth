d3b2af877f59917cce9d319204c9a17f7b17e5aa
Elisassh <elisatony021@gmail.com>
2025-04-24 13:45:52 +0400

    Update include.h

--- include/include.h
+++ include/include.h
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+
+#ifndef FonctionMain.c
+#define BDesMots.c
+
+void filtrer_par_lettre(char **mots, int nb_mots, char lettre);
+void exclure_lettre(char **mots, int nb_mots, char lettre);
+void substring_filter(char **mots, int nb_mots, char *sub);
+
 
 #define MAX_WORDS 10000
 #define WORD_LENGTH 6   // 5 lettres + '\0'
 #define LINE_SIZE 512
+
+
+#endif
+
+
